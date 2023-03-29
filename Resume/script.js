796ea8d91a98ec7db65fd1765fc9bd878aed2fad
Sowmya-Krish <sowmikrish.crazy@gmail.com>
2023-03-29 11:17:29 +0530

    day 3 task

---
+++ Resume/script.js
@@ -0,0 +1,48 @@
+let myResume = {
+  basics: {
+    name: "Sowmya K",
+    email: "sowmikrish.crazy@gamil.com",
+    phone: 9790477769,
+    degree: "B.Tech Bioinformatics",
+    location: {
+      address: "No 39 Tautanagar, West Cross 1",
+      postalCode: 641041,
+      city: "Coimbatore",
+      state: "Tamilnadu",
+      country: "India",
+    },
+    profiles: [
+      {
+        github: "https://github.com/Sowmya-Krish",
+      },
+    ],
+  },
+  education: [
+    {
+      institution: "TamilNadu Agricultural University",
+      department: "Bioinformatics",
+      cgpa: 8.2,
+    },
+  ],
+  skills: [
+    {
+      name: "javascript,html,css,ReactJS, NodeJS",
+      level: "beginner",
+    },
+  ],
+  languages: [
+    {
+      language: "Tamil,English,Malayalam",
+    },
+  ],
+  Experience: [
+    {
+      "Company Name": "VWR International",
+      "No of Years": "3 years",
+
+      "Company Name": "NobleServe",
+      "No of Years": "1.5 years",
+    },
+  ],
+};
+console.log(myResume);
